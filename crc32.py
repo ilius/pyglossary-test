4884d36edcfec1ab67018b59e04b9297282c85db
Saeed Rasooli <saeed.gnu@gmail.com>
2022-01-03 07:36:15 +0330

    add crc32.py

---
+++ crc32.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+from os.path import isdir
+import struct
+import binascii
+
+def crc32hex(bs: bytes) -> str:
+	return struct.pack('>I', binascii.crc32(bs) & 0xffffffff).hex()
+
+
+for fname in sorted(sys.argv[1:]):
+	if isdir(fname):
+		continue
+	if fname.endswith(".py"):
+		continue
+	with open(fname, mode="rb") as _file:
+		data = _file.read()
+	print(f'\t"{fname}": "{crc32hex(data)}",')
