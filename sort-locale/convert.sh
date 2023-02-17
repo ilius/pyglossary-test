8992df8de9de06d9f64ca764b8310eea1abce9d3
Saeed Rasooli <saeed.gnu@gmail.com>
2023-02-17 23:16:39 +0330

    add sort-locale/092-en-fa-alphabet-sample-sorted-latin-fa.txt

--- sort-locale/convert.sh
+++ sort-locale/convert.sh
@@ -8,4 +8,6 @@ pyglossary $NAME.txt $NAME-sorted-en.txt --sort --sort-locale en_US.UTF-8
 
 pyglossary $NAME.txt $NAME-sorted-fa.txt --sort --sort-locale fa_IR.UTF-8
 
+pyglossary $NAME.txt $NAME-sorted-latin-fa.txt --sort --sort-locale fa-u-kr-latn-arab
+# --sort-key :fa-u-kr-latn-arab
 
