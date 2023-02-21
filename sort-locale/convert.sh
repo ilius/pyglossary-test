1a686d4cb88d37678b4162d33f3237ee0cc3b3db
Saeed Rasooli <saeed.gnu@gmail.com>
2023-02-21 17:12:16 +0330

    update sort-locale/convert.sh

--- sort-locale/convert.sh
+++ sort-locale/convert.sh
@@ -4,10 +4,8 @@ NAME=092-en-fa-alphabet-sample
 
 pyglossary $NAME.txt $NAME-sorted-default.txt --sort
 
-pyglossary $NAME.txt $NAME-sorted-en.txt --sort --sort-locale en_US.UTF-8
+pyglossary $NAME.txt $NAME-sorted-en.txt --sort --sort-key :en_US.UTF-8
 
-pyglossary $NAME.txt $NAME-sorted-fa.txt --sort --sort-locale fa_IR.UTF-8
+pyglossary $NAME.txt $NAME-sorted-fa.txt --sort --sort-key :fa_IR.UTF-8
 
-pyglossary $NAME.txt $NAME-sorted-latin-fa.txt --sort --sort-locale fa-u-kr-latn-arab
-# --sort-key :fa-u-kr-latn-arab
-
+pyglossary $NAME.txt $NAME-sorted-latin-fa.txt --sort --sort-key :fa-u-kr-latn-arab
