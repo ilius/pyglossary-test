42848075c71245162cabbb67dab2affe9936bc7f
Saeed Rasooli <saeed.gnu@gmail.com>
2024-12-19 06:03:46 +0330

    wiktextract

--- wiktextract/convert.sh
+++ wiktextract/convert.sh
@@ -2,3 +2,6 @@ PG="python $HOME/pyglossary/main.py"
 
 $PG 10-kaikki-fa-PlacesInIran.jsonl 10-kaikki-fa-PlacesInIran.txt
 $PG 10-kaikki-fa-PlacesInIran.jsonl 10-kaikki-fa-PlacesInIran-category.txt --read-options=categories=1
+
+$PG 10-kaikki-fa-pos-adv.jsonl 10-kaikki-fa-pos-adv.txt
+$PG 10-kaikki-fa-pos-adv.jsonl 10-kaikki-fa-pos-adv-word_title.txt --read-options=word_title=1
