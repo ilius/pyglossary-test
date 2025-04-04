3e7eb211e102109253013e67a40c24d72039319a
Saeed Rasooli <saeed.gnu@gmail.com>
2025-04-04 03:31:43 +0330

    wiktextract: add v3

--- wiktextract/convert.sh
+++ wiktextract/convert.sh
@@ -1,9 +1,9 @@
 PG="python $HOME/pyglossary/main.py"
 
-$PG 10-kaikki-fa-PlacesInIran.jsonl 10-kaikki-fa-PlacesInIran-v2.txt
-$PG 10-kaikki-fa-PlacesInIran.jsonl 10-kaikki-fa-PlacesInIran-category-v2.txt --read-options=categories=1
+$PG 10-kaikki-fa-PlacesInIran.jsonl 10-kaikki-fa-PlacesInIran-v3.txt
+$PG 10-kaikki-fa-PlacesInIran.jsonl 10-kaikki-fa-PlacesInIran-category-v3.txt --read-options=categories=1
 
-$PG 10-kaikki-fa-pos-adv.jsonl 10-kaikki-fa-pos-adv-v2.txt
-$PG 10-kaikki-fa-pos-adv.jsonl 10-kaikki-fa-pos-adv-word_title-v2.txt --read-options=word_title=1
+$PG 10-kaikki-fa-pos-adv.jsonl 10-kaikki-fa-pos-adv-v3.txt
+$PG 10-kaikki-fa-pos-adv.jsonl 10-kaikki-fa-pos-adv-word_title-v3.txt --read-options=word_title=1
 
-$PG 03-kaikki-fa-selection.jsonl 03-kaikki-fa-selection-v2.txt
+$PG 03-kaikki-fa-selection.jsonl 03-kaikki-fa-selection-v3.txt
