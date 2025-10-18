28e4e964037a7eb18b3343e39c3dd0d29d869850
Saeed Rasooli <saeed.gnu@gmail.com>
2025-10-19 02:15:20 +0330

    edict2: add *v2*.txt

--- edict2/convert.sh
+++ edict2/convert.sh
@@ -4,11 +4,11 @@ set -x
 
 PG="python $HOME/pyglossary/main.py"
 
-$PG 024-cedict.u8 024-cedict.txt
+$PG 024-cedict.u8 024-cedict-v2.txt
 
-$PG 024-cedict.u8 024-cedict-trad.txt --read-options traditional_title=True
+$PG 024-cedict.u8 024-cedict-v2-trad.txt --read-options traditional_title=True
 
-$PG 024-cedict.u8 024-cedict-nocolor.txt --read-options colorize_tones=False
+$PG 024-cedict.u8 024-cedict-v2-nocolor.txt --read-options colorize_tones=False
 
-$PG 024-cedict.u8 024-cedict-links.txt --read-options link_references=True
+$PG 024-cedict.u8 024-cedict-v2-links.txt --read-options link_references=True
 
